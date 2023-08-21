efd7aadaa17c63e9cdf2023437738e3e406f8dc4
Ville Pikaloff <ville.pikaloff@gmail.com>
2023-08-21 09:01:28 +0300

    Completed anecdotes part 1
    Exercises up to 1.14, moving on to Part 2 of the course next

--- app/components/Anecdotes.tsx
+++ app/components/Anecdotes.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export const Anecdotes = () => {
   const anecdotes = [
@@ -13,18 +13,79 @@ export const Anecdotes = () => {
     "The only way to go fast, is to go well.",
   ];
 
-  const [selected, setSelected] = useState(0);
+  const [points, setPoints] = useState(Array(8).fill(0));
+
+  const [selected, setSelected] = useState<number>(0);
+
+  const [mostVotes, setMostVotes] = useState<number>(0);
+
+  //Pseudo-randomly selects an anecdote from the array using Math.random()
 
   function nextAnecdote() {
     setSelected(Math.floor(Math.random() * anecdotes.length));
   }
 
-  console.log(anecdotes.length)
+  //Adds a point to the points array for the currently displayed anecdote
+
+  function voteForAnecdote() {
+    const newVotes = [...points];
+    newVotes[selected] += 1;
+    setPoints(newVotes);
+  }
+
+  //Goes through the points array and returns the index of the biggest number
+  //Index is then used to render to anecdote and the point value from their respective arrays
+
+  function findMostVoted() {
+    let mostVoted = points[0];
+    let index = 0;
+
+    for (let i = 1; i < points.length; i++) {
+      if (points[i] > mostVoted) {
+        mostVoted = points[i];
+        index = i;
+      }
+    }
+
+    setMostVotes(index);
+  }
+
+  //useEffect to keep track of the most voted anecdote, runs the function to find most votes
+  //every time the points- array is changed
+
+  useEffect(() => {
+    findMostVoted();
+  }, [points]);
 
   return (
     <>
-      <div>{anecdotes[selected]}</div>
-      <button onClick={() => nextAnecdote()} className="bg-slate-600 rounded-xl p-2">Next anecdote</button>
+      <div className="flex flex-col p-5">
+        <h2 className="text-xl py-2 font-bold">Anecdote of the day</h2>
+        <p>{anecdotes[selected]}</p>
+        <p className="font-bold">has {points[selected]} votes</p>
+
+        <div className="flex">
+          <div className="p-2">
+            <button
+              onClick={() => voteForAnecdote()}
+              className="bg-slate-600 rounded-xl p-2 min-w-[8dvw]"
+            >
+              Vote
+            </button>
+          </div>
+          <div className="p-2">
+            <button
+              onClick={() => nextAnecdote()}
+              className="bg-slate-600 rounded-xl p-2 min-w-[8dvw]"
+            >
+              Next anecdote
+            </button>
+          </div>
+        </div>
+        <h2 className="text-xl py-2 font-bold">Anecdote with most votes</h2>
+        <p>{anecdotes[mostVotes]}</p>
+        <p className="font-bold">has {points[mostVotes]} votes</p>
+      </div>
     </>
   );
 };
